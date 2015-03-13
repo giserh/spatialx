e98d5740a5fc46edda539e51dbd683d12a8dac99
Remi Louf <remilouf@gmail.com>
2015-03-13 11:35:37 +0000

    Started to code the intersection continuation algorithm by Porta et al. to find segments in spatial network.

---
+++ spatialx/information/intersection_continuation.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""intersection_continuation.py
+
+Implements the intersection continuation negotiation (ICN) algorithm proposed by
+the authors of [1]_
+
+.. [1] Porta S, Crucitti P, Latora V  
+       The network analysis of urban streets: a dual approach. 
+       Physica A: Statistical Mechanics and its Applications 369(2):853–866
+       (2006).
+"""
